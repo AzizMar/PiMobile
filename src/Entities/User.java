8a354b0f2044a3650bdb6c1f44d64a5d07e81a37
aziz <azizxmar@gmail.com>
2017-06-29 15:38:55 +0100

    Login completed

--- src/Entities/User.java
+++ src/Entities/User.java
@@ -5,10 +5,91 @@
  */
 package Entities;
 
+import com.codename1.ui.Image;
+
 /**
  *
  * @author azizx
  */
 public class User {
+    
+    private int id;
+    private String username;
+    private String email;
+    private String password;
+    private String nom,prenom;
+    private Image imgProfile;
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public User() {
+    }
+
+    
+    
+    
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public Image getImgProfile() {
+        return imgProfile;
+    }
+
+    public void setImgProfile(Image imgProfile) {
+        this.imgProfile = imgProfile;
+    }
+    
+    
+    
     
 }
--- src/GUI/visiteur/Login.java
+++ src/GUI/visiteur/Login.java
@@ -5,14 +5,27 @@
  */
 package GUI.visiteur;
 
+import Entities.User;
 import com.codename1.components.ImageViewer;
+import com.codename1.io.CharArrayReader;
+import com.codename1.io.ConnectionRequest;
+import com.codename1.io.JSONParser;
+import com.codename1.io.NetworkEvent;
+import com.codename1.io.NetworkManager;
 import com.codename1.ui.Button;
 import com.codename1.ui.Container;
 import com.codename1.ui.Form;
 import com.codename1.ui.Label;
 import com.codename1.ui.TextField;
+import com.codename1.ui.events.ActionEvent;
+import com.codename1.ui.events.ActionListener;
 import com.codename1.ui.util.Resources;
 import com.codename1.ui.util.UIBuilder;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 
 /**
  *
@@ -73,10 +86,84 @@ public class Login {
         });
         
         
+            btnLogin.addActionListener((ActionEvent e)->{
+                
+                ConnectionRequest con = new ConnectionRequest();
+                con.setUrl("http://www.azizxmar.heliohost.org/userSelect.php");
+        
+        
+        
+        con.addResponseListener((NetworkEvent evt) -> {
             
+            String rep = new String(con.getResponseData());
+            System.out.println(rep);
+                    ArrayList<User> usersList = getListUser(rep);
+                    
+//                    usersList.forEach(item->{
+//                        
+//                       
+//                        });
+                    for (User u : usersList) {
+                        
+                         System.out.println(u.getPassword());
+                         
+                          if (u.getUsername().equals(txtLogin.getText()) && u.getPassword().startsWith(txtPwd.getText())) {
+                            
+                            //  GUI.user.Home uh = new GUI.user.Home();
+                        System.out.println("login success²");
+
+                            
+                            break;
+                        }
+                        
+                        else{
+                            System.out.println("login Failed²");
+                        }
+                      }
+                            
+                            
+                   System.out.println(usersList.size());
+                            
+//                System.out.println(getListEtudiant(new String(con.getResponseData())));
+//                sp.setText(getListEtudiant(new String(con.getResponseData())) + "");
+//                hi.refreshTheme();
+//                hi.show();
+                });
+        NetworkManager.getInstance().addToQueue(con);
+                
+            });
         
         
         
+    }
+    
+        public ArrayList<User> getListUser(String json) {
+        
+        ArrayList<User> listUsers = new ArrayList<>();
+
+        try {
+
+            JSONParser j = new JSONParser();
+           
+
+            Map<String, Object> users = j.parseJSON(new CharArrayReader(json.toCharArray()));
+
+            List<Map<String, Object>> list = (List<Map<String, Object>>) users.get("utilisateur");
+
+            for (Map<String, Object> obj : list) {
+                User u = new User();
+                u.setUsername(obj.get("username").toString());
+                u.setEmail(obj.get("email").toString());
+                u.setPassword(obj.get("password").toString());
+                listUsers.add(u);
+              //  System.out.println(obj.get("username").toString());
+
+            }
+
+        } catch (IOException ex) {
+         }
+        return listUsers;
+
     }
     
     public void showForm(){
